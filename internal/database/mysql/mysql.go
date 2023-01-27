37cb58c6675d56573dcb22846a01437b7e6b14eb
alwi09 <alwi.irfani1927@gmail.com>
2023-01-27 13:46:05 +0700

    chore: change connectDB use hard code

--- internal/database/mysql/mysql.go
+++ internal/database/mysql/mysql.go
@@ -2,26 +2,17 @@ package mysql
 
 import (
 	"database/sql"
-	"encoding/json"
-	"github.com/sirupsen/logrus"
-	"os"
-	"todolist_mux/internal/configuration"
 )
 
 func ConnectDB() (*sql.DB, error) {
-
-	var config configuration.Config
-	db, err := os.Open("config.json")
+	dbDriver := "mysql"
+	dbUser := "alwi09"
+	dbPass := "alwiirfani11"
+	dbName := "rest_full_api_todolist"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	if err != nil {
-		logrus.Fatal(err.Error())
+		panic(err.Error())
 	}
 
-	defer db.Close()
-
-	jsonParse := json.NewDecoder(db)
-	if err = jsonParse.Decode(&config); err != nil {
-		logrus.Fatal(err.Error())
-	}
-
-	return nil, err
+	return db, err
 }
