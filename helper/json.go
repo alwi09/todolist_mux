f7a9c4c648034b87da7d3528787b029e470aa02d
alwi09 <alwi.irfani1927@gmail.com>
2023-01-27 13:43:23 +0700

    Add: return value error

--- helper/json.go
+++ helper/json.go
@@ -5,15 +5,20 @@ import (
 	"net/http"
 )
 
-func ReadFromRequestBody(r *http.Request, result interface{}) {
+func ReadFromRequestBody(r *http.Request, result interface{}) error {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(result)
 	PanicIfError(err)
+
+	return nil
 }
 
-func WriteFromRequestBody(w http.ResponseWriter, response interface{}) {
+func WriteFromRequestBody(w http.ResponseWriter, response interface{}) error {
 	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(response)
 	PanicIfError(err)
+
+	return nil
 }
