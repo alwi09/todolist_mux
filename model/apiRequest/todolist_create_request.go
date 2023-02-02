f450cdb34cc6ab5f8321f11da56ba812702137f3
alwi09 <alwi.irfani1927@gmail.com>
2023-02-02 21:53:36 +0700

    chore: delete max=99 in validate title

--- model/apiRequest/todolist_create_request.go
+++ model/apiRequest/todolist_create_request.go
@@ -1,6 +1,6 @@
 package apiRequest
 
 type TodolistCreateRequest struct {
-	Title       string `validate:"required,min=2,max=99" json:"title"`
+	Title       string `validate:"required,min=2" json:"title"`
 	Description string `validate:"required,min=2" json:"description"`
 }
